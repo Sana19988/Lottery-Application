6db2c657eddb3369109a8fd362677e04dfacb4eb
Sana19988 <tesica84@gmail.com>
2024-03-09 15:03:56 +0100

    Connect mssql database

--- Lottery Application/Data/LotteryDbContext.cs
+++ Lottery Application/Data/LotteryDbContext.cs
@@ -1,6 +1,5 @@
 using Lottery_Application.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace Lottery_Application.Data;
 
@@ -12,47 +11,8 @@ public class LotteryDbContext : DbContext
     {
         if (!optionsBuilder.IsConfigured)
         {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            string connectionString = configuration.GetConnectionString("LotteryDbConnection");
-            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            string connectionString = "Server=localhost\\SQLEXPRESS;Database=lotteryDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
-
-
-// provera za db?? 
-/*public class LotteryDbContext : DbContext
-    {
-        public DbSet<LotteryDraw> DrawHistory { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                string connectionString = "Server=localhost;Database=LotteryDB;Uid=root;Pwd=";
-
-                if (string.IsNullOrEmpty(connectionString))
-                {
-                    throw new InvalidOperationException("Connection string is not configured.");
-                }
-
-                try
-                {
-                    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-                    using (var connection = new MySqlConnection(connectionString))
-                    {
-                        connection.Open();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new InvalidOperationException("Database connection error.", ex);
-                }
-            }
-        }
-    } 
- */
--- Lottery Application/obj/Debug/net8.0/Lottery Application.AssemblyInfo.cs
+++ Lottery Application/obj/Debug/net8.0/Lottery Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lottery Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c33360b8f9c0a2fc456a2a06eacaddb0f30d05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lottery Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lottery Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
